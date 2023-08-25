d43f1ea4308b5f3bb1288ba4ac196e0941d63753
LimitlessJesse <jessewu1999@gmail.com>
2023-08-25 16:29:06 -0500

    Added db model and local db

---
+++ MusicPlayer/Models/Song.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicPlayer.Models
+{
+    public class Song
+    {
+        [Key]
+        public string SourceId { get; set; }
+
+        [Required]
+        public string SongName { get; set; }
+
+        [Required]
+        public string Artist { get; set; }
+
+        public List<Playlist> Playlists { get; } = new();
+
+    }
+}
