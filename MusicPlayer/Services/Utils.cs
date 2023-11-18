095e527c9884c772a61a236c4d29592d3c43f37e
LimitlessJesse <jessewu1999@gmail.com>
2023-11-17 18:53:44 -0600

    Changed how the primary key of the song is stored, now can directly copy link from YouTube url

--- MusicPlayer/Services/Utils.cs
+++ MusicPlayer/Services/Utils.cs
@@ -2,12 +2,12 @@
 {
     public class Utils
     {
-        private const string YOUTUBE_STARTER = "https://www.youtube.com/embed/";
-        private const int EMBED_LINK_LENGTH = 31;
+        private const string YOUTUBE_STARTER = "https://www.youtube.com/watch?v=";
+        private const int SOURCEID_LENGTH = 11;
 
         public static string SongSourceIdExtrator(string embedUrl)
         {
-            return embedUrl.Substring(embedUrl.IndexOf(YOUTUBE_STARTER) + YOUTUBE_STARTER.Length, EMBED_LINK_LENGTH);
+            return embedUrl.Substring(embedUrl.IndexOf(YOUTUBE_STARTER) + YOUTUBE_STARTER.Length, SOURCEID_LENGTH);
         }
 
         public static string EmbedUrlBuilder(string songSourceId)
